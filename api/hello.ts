5bd69caa0b240f8eb6a36cd0fe40e2f3b7ad0fc3
jeshu2002 <71264878+jeshu2002@users.noreply.github.com>
2024-03-03 12:33:33 +0530

    up

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
   return res.json(
    [ {
       "id": 1,
