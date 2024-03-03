f9ff10e02ea2a754962e4eea49c48a8af4a4f1d7
jeshu2002 <71264878+jeshu2002@users.noreply.github.com>
2024-03-03 12:09:25 +0530

    up

--- api/hello.ts
+++ api/hello.ts
@@ -3,14 +3,23 @@ import fs from 'fs-extra';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
+    // Log to see if the function is being called
+    console.log('Function is called.');
+
     // Read the models.json file asynchronously
-    const modelsData = await fs.readFile('api/models.jsonn', 'utf-8');
-    const models = JSON.parse(modelsData);
+    const modelsData = await fs.readFile('api/models.json', 'utf-8');
     
+    // Log to see if reading the file is successful
+    console.log('File read successfully.');
+
+    const models = JSON.parse(modelsData);
+
     // Return the models data as JSON
     return res.json(models);
   } catch (error) {
-    console.error('Error reading models.json:', error);
+    // Log the error to see what went wrong
+    console.error('Error:', error);
+
     // Return an error response if reading the file fails
     return res.status(500).json({ error: 'Internal Server Error' });
   }
